13a40d3e075fc9160647b8f140dbc8b2d601a2ec
Danilo Domingos <danilo.domingos02@gmail.com>
2018-11-18 16:07:13 -0200

    [AULA 2] - Adicionando métodos.

--- conta.py
+++ conta.py
@@ -1,8 +1,17 @@
 class Conta:
-   
-   def __init__(self, numero, titular, saldo, limite):
-       print("Construindo  objeto")
-       self.numero = numero
-       self.titular = titular
-       self.saldo = saldo
-       self.limite = limite
+
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo  objeto")
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
+
+    def extrato(self):
+        print(f"Saldo {self.saldo} do titular {self.titular}")
+    
+    def deposita(self, valor):
+        self.saldo += valor
+    
+    def saca(self, valor):
+        self.saldo -= valor
