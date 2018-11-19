190cab96214f69a7d21e9393c5392f02b600a0f5
Danilo Domingos <danilo.domingos02@gmail.com>
2018-11-19 16:20:52 -0200

    [AULA 6] - Adicionando métodos privados && métodos statics

--- conta.py
+++ conta.py
@@ -14,8 +14,31 @@ class Conta:
         self.__saldo += valor
     
     def saca(self, valor):
-        self.__saldo -= valor
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            return (f"O valor {valor} ultrapassa o limite")
+
+    def __pode_sacar(self, valor):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor <= valor_disponivel_a_sacar
     
     def  transfere(self, valor, conta):
         self.saca(valor)
         conta.deposita(valor)
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+    
+    @staticmethod
+    def codigo_banco():
+        return "001"
+    
+    @staticmethod
+    def codigos_bancos():
+        return {'BB':'001', 'Caixa' : '101', 'Bradesco': '237'}
